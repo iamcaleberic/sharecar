b36ab849ec3fc3fc59f2c6a42a1b8f2d04d5e964
Eric Caleb <calebme@mail.com>
2016-05-05 13:07:03 +0100

    Static assets and H

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,6 +5,7 @@ Rails.application.configure do
   config.cache_classes = true
   config.serve_static_files = true
 
+  config.assets.compile = true
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
